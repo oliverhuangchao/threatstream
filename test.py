819924d54024f7c49f19fe347c141dc82272eb83
oliverhuangchao <chaoh@g.clemson.edu>
2015-08-09 17:16:38 -0700

    change some parts of test.py

--- test.py
+++ test.py
@@ -1,48 +1,12 @@
-# description
-#There is an array of integers. Please write a piece of code to find 3 elements whose sum is zero.
+import psycopg2
 
-# basic idea:
-# 1. firstly sort this array
-# 2. Then choose a integer(x) from index 0 to the last
-#     a. use two pointers (a,b) starting from both sides to the mid (except x)
-#     b. add num[a],nums[b] and x together and compare to 0
-#         if bigger b--
-#         if smaller b++
-#         if equal store this output
-# 3. do something corner case to accerlate this procedure
-def sum2zero(nums):
-    nums.sort()
-    res = []
-    size = len(nums)
-    path = []
-    i = 0
-    while i<size:
-        a = i+1
-        b = size-1
-        while a<b:
-            tmp = nums[a]+nums[b]+nums[i]
-            if tmp == 0:
-                path.append(nums[i])
-                path.append(nums[a])
-                path.append(nums[b])
-                res.append(path)
-                while a<b and path[1] == nums[a]:
-                    a += 1
-                while a<b and path[2] == nums[b]:
-                    b -= 1
-                path = []
-            else:
-                if tmp < 0:
-                    a += 1
-                else:
-                    b -= 1
-        while i + 1 < size and nums[i+1] == nums[i]:
-            i += 1
-        i += 1
-    return res
+try:
+    conn = psycopg2.connect("dbname='exampledb' user='dbuser' host='localhost' password='hc'")
+except:
+    print "I am unable to connect to the database"
 
-nums = [1,0,-1,-1,-1,-1,0,1,1,1]
-print nums
-res = sum2zero(nums)
-for item in res:
-    print item
+cur = conn.cursor()
+cur.execute("SELECT * from second")
+rows = cur.fetchall()
+#print "\nShow me the databases:\n"
+print rows
